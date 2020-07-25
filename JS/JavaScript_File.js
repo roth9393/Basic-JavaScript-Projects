05f4dda191aea3e619e50ead7f9a16e19458967f
Salvador Castaneda <311.salhd311@gmail.com>
2020-07-25 15:33:05 -0600

    Initial commit

---
+++ JS/JavaScript_File.js
@@ -0,0 +1,10 @@
+var A = "Beer Lover!";
+window.alert(A);
+
+document.write("List told Bart, \"that he has sticky!");
+
+document.write("\"Test"
++ " test");
+
+var B = "This is a crazy as" + "test.";
+document.write(B);
